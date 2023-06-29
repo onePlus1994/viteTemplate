bf6c00babaa2b3f3b45dbbaaa874556ee92ba519
onePlus1994 <948846529@qq.com>
2023-06-29 22:24:56 +0800

    添加头像

--- src/components/headerInformation/index.vue
+++ src/components/headerInformation/index.vue
@@ -1,12 +1,31 @@
 <template>
     <div class="headerBody">
         <div class="img"></div>
-        <div>123465</div>
+        <div>
+            <el-dropdown ref="dropdown1" trigger="contextmenu" class="margStyle">
+                <el-avatar shape="circle" :size="40" src="src/assets/image/head.png" @click="showClick" />
+                <template #dropdown>
+                    <el-dropdown-menu>
+                        <el-dropdown-item>Action 1</el-dropdown-item>
+                        <el-dropdown-item>Action 2</el-dropdown-item>
+                        <el-dropdown-item>Action 3</el-dropdown-item>
+                        <el-dropdown-item disabled>Action 4</el-dropdown-item>
+                        <el-dropdown-item divided>Action 5</el-dropdown-item>
+                    </el-dropdown-menu>
+                </template>
+            </el-dropdown>
+            <el-avatar shape="square" :size="40" src="src/assets/image/head.png" />
+        </div>
     </div>
 </template>
   
 <script setup lang="ts">
-
+import { ref } from 'vue'
+const dropdown1 = ref()
+const showClick = () => {
+    if (!dropdown1.value) return
+    dropdown1.value.handleOpen()
+}
 </script>
   
 <style lang="less" scoped>
@@ -23,6 +42,10 @@
     background-position: center;
     height: 60px;
     width: 200px;
+}
+
+.margStyle {
+    margin-right: 10px;
 }
 </style> 
   
