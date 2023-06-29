9c7247fe269f386028b98ac00f12c07b624f635a
onePlus1994 <948846529@qq.com>
2023-06-29 21:59:47 +0800

    添加logo

--- src/components/headerInformation/index.vue
+++ src/components/headerInformation/index.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-        12346
+    <div class="headerBody">
+        <div class="img"></div>
+        <div>123465</div>
     </div>
 </template>
   
@@ -8,5 +9,20 @@
 
 </script>
   
-<style lang="less" scoped></style> 
+<style lang="less" scoped>
+.headerBody {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.img {
+    background-image: url(@/assets/image/logo.png);
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center;
+    height: 60px;
+    width: 200px;
+}
+</style> 
   
